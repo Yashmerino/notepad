0d3c5e4e5fc5c7fa7027d6edae0aa82060de5e45
Artiom Bozieac <artiomytprofile@gmail.com>
2021-08-03 14:21:06 +0300

    Added open file button functionality

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "headers/mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,10 +10,35 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     this->setCentralWidget(ui->textbox);
+    this->setWindowTitle("Notepad");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString file_name = QFileDialog::getOpenFileName(this, "Open a file"); // Get open file name
+    QFile file(file_name);
+
+    notepad.currentFile_name = file_name;
+
+
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)) // If can't read file then error
+    {
+        QMessageBox::warning(this, "Error", "Cannot open file: " + file.errorString());
+    }
+    else // Else set window title to the file name and paste all the content in textbox
+    {
+        setWindowTitle(file_name);
+        QTextStream inputText(&file);
+
+        QString textFile = inputText.readAll();
+
+        ui->textbox->setText(textFile);
+    };
 }
 
